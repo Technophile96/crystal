441ae341ab4adcd2b4d25551356ea6b87d60e417
Muthukumaran <19eucs096@skcet.ac.in>
2022-05-12 01:21:38 +0530

    final

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './dashboard.css'
 import logo from './logopng.png'
-import axios from 'axios' 
-import { useState } from 'react'
+// import axios from 'axios' 
+// import { useState } from 'react'
 
 const dashboard = () => {
     // retriveName();
