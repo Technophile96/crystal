afb2c41fda9170e5d3cca074d32912cff7f16d38
Muthukumaran <19eucs096@skcet.ac.in>
2022-05-12 01:29:28 +0530

    s

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import './dashboard.css'
 import logo from './logopng.png'
-// import axios from 'axios' 
-// import { useState } from 'react'
+import axios from 'axios' 
+import { useState } from 'react'
 
 const dashboard = () => {
-    // retriveName();
-    // const [teahList, setList] = useState([]); 
-    // const retriveName = (e) =>{
-    //     e.preventDefault();
-    //     axios.post("https://crystal-delta-banking.herokuapp.com/api/name",{
-    //         CustID:localStorage.getItem('Name'),
-    //     }).then((response)=>{
-    //        console.log(response);
-    //        setList(response.data);
-    //     });
-    //   };
+
+    const [teahList, setList] = useState([]); 
+    const retriveName = (e) =>{
+        const id=localStorage.getItem('Name');
+        e.preventDefault();
+        axios.post("https://crystal-delta-banking.herokuapp.com/api/name",{
+            CustID:id,
+        }).then((response)=>{
+           console.log(response);
+           setList(response.data);
+        });
+      };
 
   return (
       <>
         <nav>
-            <div className='div-header'>
+            <div className='div-header' onLoad={retriveName}>
                 <div>
                     <img src={logo} className='nhm' alt="Description of Home"/>
                 </div>
@@ -30,11 +31,11 @@ const dashboard = () => {
             </div>
         </nav>
         <main className='main'>
-        {/* {teahList.map((item, index)=>{
+        {teahList.map((item, index)=>{
             return(
             <h1 className='username'>Greetings {item.custname}</h1>
             )
-        })} */}
+        })}
            <h1 className='username'>Greetings {localStorage.getItem('Name')}</h1>
             <div className='detailbox'>
                 <div className='options'>
