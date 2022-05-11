49692ae8ad3d0e197ff11a89572f8226bf2bc1cd
Muthukumaran <19eucs096@skcet.ac.in>
2022-05-11 19:41:16 +0530

    toast final try

--- src/pages/Loginpart.js
+++ src/pages/Loginpart.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import toast from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+toast.configure();
 const Loginpart = () => {
    
   let navigate = useNavigate(); 
