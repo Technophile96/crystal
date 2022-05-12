c487e649fe003a31f4b3181a0a7e9d57129c0ebb
Muthukumaran <19eucs096@skcet.ac.in>
2022-05-12 13:34:45 +0530

    cd

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,6 +18,16 @@ const Dashboard = () => {
            setList(response.data);
         });
       };
+      const retriveTransactions = (e) =>{
+        const id=localStorage.getItem('Name');
+        e.preventDefault();
+        axios.post("https://crystal-delta-banking.herokuapp.com/api/transactions",{
+            CustID:id,
+        }).then((response)=>{
+           console.log(response);
+           setList(response.data);
+        });
+      };
 
       const [isOpen, setIsOpen] = useState(false);
  
@@ -68,9 +78,13 @@ const Dashboard = () => {
                         <button className='buttoon' onClick={togglePopup}>View Statement</button>
                         {isOpen && <Popup
                         content={<>
+                         {teahList.map((item, index)=>{
+                          return (
+                            <>
                             <b>Design your Popup</b>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <button>Test button</button>
+                            </>
+                            )
+                         })}
                         </>}
                         handleClose={togglePopup}
                         />}
