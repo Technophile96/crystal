b3d4b3384741169993342f9a07906cb9b8ead6bc
Muthukumaran <19eucs096@skcet.ac.in>
2022-05-12 12:05:38 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,6 +18,12 @@ const Dashboard = () => {
         });
       };
 
+      const [isOpen, setIsOpen] = useState(false);
+ 
+      const togglePopup = () => {
+        setIsOpen(!isOpen);
+      }
+
   return (
       <>
         <nav>
@@ -35,7 +41,14 @@ const Dashboard = () => {
         <main className='main'>
         
             <h1 className='username'>Greetings {item.custname}!</h1>
-        
+            {isOpen && <Popup
+      content={<>
+        <b>Design your Popup</b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>Test button</button>
+      </>}
+      handleClose={togglePopup}
+    />}
             <div className='detailbox'>
                 <div className='options'>
                     <button className='btn1 active'>Account Summary</button>
@@ -58,7 +71,7 @@ const Dashboard = () => {
                                 <p>IFSC code : {item.IFSC}</p>
                             </div>
                         </div>
-                        <button className='buttoon'>View Statement</button>
+                        <button className='buttoon' onClick={togglePopup}>View Statement</button>
                     </div>
                 </div>
             </div>
